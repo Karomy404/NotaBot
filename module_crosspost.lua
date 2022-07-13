e8bc8cd1c153d1f7a7d26af0c59f631d6dabb5a4
Ben4444 <91879977+Karomy404@users.noreply.github.com>
2022-07-13 12:02:43 +0200

    Update module_crosspost.lua

--- module_crosspost.lua
+++ module_crosspost.lua
@@ -1,3 +1,18 @@
+-- Copyright (C) 2018 Jérôme Leclercq
+-- This file is part of the "Not a Bot" application
+-- For conditions of distribution and use, see copyright notice in LICENSE
+
+local client = Client
+local discordia = Discordia
+local bot = Bot
+local enums = discordia.enums
+
+Module.Name = "crosspost"
+
+function Module:CheckPermissions(member)
+	return member:hasPermission(enums.permission.manageChannels)
+end
+
 local Message = discordia.class.classes.Message
 local CrosspostEndpoint = "/channels/%s/messages/%s/crosspost"
 
